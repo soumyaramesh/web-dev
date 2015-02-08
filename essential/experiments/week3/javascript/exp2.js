41a8ff90eba9eef5796e80f8659858cb81363af4
soumyaramesh <soumya@ccs.neu.edu>
2015-02-08 00:43:00 -0500

    completed exp2

---
+++ essential/experiments/week3/javascript/exp2.js
@@ -0,0 +1,8 @@
+﻿var top = "stack1";
+function move(curr) {
+    oldstack = document.getElementById(top).style;
+    newstack = document.getElementById(curr).style;
+    oldstack.zIndex = '0';
+    newstack.zIndex = '10';
+    top = curr;
+}
