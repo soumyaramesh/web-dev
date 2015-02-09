15c7d77b32291903d8dff66d14c82b6eebf1af82
soumyaramesh <soumya@ccs.neu.edu>
2015-02-08 23:04:55 -0500

    completed 5 exp

---
+++ essential/experiments/week3/javascript/exp4.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#blue").mouseenter(function () {
+        $("#blue").fadeTo('fast', 1)
+    });
+    $("#blue").mouseleave(function () {
+        $("#blue").fadeTo('fast', 0.5)
+    });
+    $("#orange").click(function () {
+        $("#orange").fadeOut('slow');
+    });
+});
---
+++ essential/experiments/week3/javascript/exp5.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#button").click(function () {
+        var li = $('input[name=listItem]').val();
+        $(".list").append('<div class="delItem">' + li + '</div>');
+    });
+    
+    $(document).on('click', '.delItem', function () {
+        $(this).remove()
+    });
+});
