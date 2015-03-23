a71e8d5c421332510bd7cb74b669563ca38c5b7f
soumyaramesh <soumya@ccs.neu.edu>
2015-03-23 04:10:56 -0400

    week8

---
+++ essential/experiments/week8/nav.js
@@ -0,0 +1,8 @@
+﻿app.controller("NavCtrl", function ($scope, $http, $location) {
+    $scope.logout = function () {
+        $http.post("/logout")
+        .success(function () {
+            $location.url("/home");
+        });
+    }
+});
---
+++ essential/experiments/week8/week8App.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module('PassportApp', ['ngRoute']);
+
+app.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+    .when('/home', {
+        templateUrl: 'views/home/home.html'
+    })
+    .when('/profile', {
+        templateUrl: 'views/profile/profile.html',
+        resolve: {
+            loggedin: checkLoggedin
+        }
+    })
+     .when('/login', {
+         templateUrl: 'views/login/login.html',
+         controller: 'LoginCtrl'
+     })
+    .when('/register', {
+        templateUrl: 'views/register/register.html',
+        controller: 'RegisterCtrl'
+    })
+
+    .otherwise({
+        redirectTo: '/home'
+    });
+});
+
+var checkLoggedin = function ($q, $timeout, $http, $location, $rootScope) {
+    var deferred = $q.defer();
+    $http.get('/loggedin').success(function (user) {
+        $rootScope.errorMessage = null;
+        if(user!='0')
+        {
+            $rootScope.currentUser = user;
+            deferred.resolve();
+        }
+        else {
+            $rootScope.errorMessage = "Please log in";
+            deferred.reject();
+            $location.url('/login');
+        }
+    })
+}
+
+
+
+
